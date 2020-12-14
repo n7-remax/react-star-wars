82a7393763db40aa2bf1ab8b0800b132587e536c
n7-remax <ditritusa@gmail.com>
2020-12-14 20:48:28 +0200

    remade planets component

--- src/components/Planets/Planets.js
+++ src/components/Planets/Planets.js
@@ -13,6 +13,7 @@ class Planets extends Component {
     prevPage: null,
     currentPage: 1,
     planets: [],
+    selectedPlanet: [],
   };
 
   getPlanets = async (page) => {
@@ -22,6 +23,7 @@ class Planets extends Component {
 
   componentDidMount() {
     this.getPlanets(`planets/?page=${this.state.initialPage}`).then((res) => {
+      console.log(res);
       this.setState({
         count: res.data.count,
         nextPage: res.data.next,
@@ -29,10 +31,12 @@ class Planets extends Component {
         planets: res.data.results,
       });
     });
+    this.onPlanetSelect(1);
   }
   // if currentPage is changed, update component
   componentDidUpdate(nextProps, nextState) {
     if (this.state.currentPage !== nextState.currentPage) {
+      console.log("updated");
       this.getPlanets(`planets/?page=${this.state.currentPage}`).then((res) => {
         this.setState({
           count: res.data.count,
@@ -43,6 +47,15 @@ class Planets extends Component {
       });
     }
   }
+  // get selected planet from api
+  onPlanetSelect = (id) => {
+    this.getPlanets(`planets/${id}/`).then((res) => {
+      console.log(res);
+      this.setState({
+        selectedPlanet: res.data,
+      });
+    });
+  };
   // merge url param props to trigger component update
   static getDerivedStateFromProps(props, state) {
     return {
@@ -52,9 +65,13 @@ class Planets extends Component {
 
   render() {
     const maxPages = Math.ceil(this.state.count / 10);
-    const PlanetsList = this.state.planets.slice(0, 4).map((planet, i) => {
+    const PlanetsList = this.state.planets.slice(0, 5).map((planet, i) => {
       return (
-        <div className="planets-item" key={i}>
+        <div
+          className="planets-item"
+          key={i}
+          onClick={() => this.onPlanetSelect(planet.url.match(/(\d+)/)[0])}
+        >
           {/* <Link to={`/react-star-wars/planet/${planet.url.match(/(\d+)/)[0]}`} className="card-link">
                     </Link> */}
           <img src={tattoine} alt={planet.name} />
@@ -67,6 +84,39 @@ class Planets extends Component {
     });
     return (
       <div className="planets">
+        <div className="planets-preview">
+          <img src={tattoine} alt={this.state.selectedPlanet.name} />
+          <div className="planets-preview-desc">
+            <h2>{this.state.selectedPlanet.name}</h2>
+            <ul>
+              <li>Terrain: {this.state.selectedPlanet.terrain}</li>
+              <li>Diameter: {this.state.selectedPlanet.diameter}</li>
+              <li>Gravity: {this.state.selectedPlanet.gravity}</li>
+              <li>
+                Orbital Period: {this.state.selectedPlanet.orbital_period}
+              </li>
+              <li>
+                Rotation Period: {this.state.selectedPlanet.rotation_period}
+              </li>
+              <li>Surface Water: {this.state.selectedPlanet.surface_water}</li>
+              <li>Population: {this.state.selectedPlanet.orbital_period}</li>
+            </ul>
+            <div className="planets-preview-btn">
+              <Link
+                to={
+                  this.state.selectedPlanet.url
+                    ? `/react-star-wars/planet/${
+                        this.state.selectedPlanet.url.match(/(\d+)/)[0]
+                      }`
+                    : `/react-star-wars/planet/1`
+                }
+                className="trans-rt-lt"
+              >
+                Famous Residents
+              </Link>
+            </div>
+          </div>
+        </div>
         <div className="planets-list">
           <Link
             to={`/react-star-wars/planets/${
