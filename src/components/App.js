df945d9821020673e4a0ec1881864cc575b4f2c2
n7-remax <ditritusa@gmail.com>
2020-12-16 16:30:59 +0200

    removed welcomepage and rewrote url

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import { Route } from "react-router-dom";
 import Planets from "./Planets/Planets";
 import FamousResidents from "./Planets/FamousResidents/FamousResidents";
 import Header from "./Header/Header";
-import WelcomePage from "./WelcomePage/WelcomePage";
 
 import "../style/style.scss";
 
@@ -10,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Route exact path="/react-star-wars/" component={WelcomePage} />
       <Route exact path="/react-star-wars/planets/:page" component={Planets} />
       <Route
         exact
