2a0b86eafcc9f758f547724ace0933a7bf413609
n7-remax <ditritusa@gmail.com>
2020-12-16 17:43:01 +0200

    added spiners on data load for planets component

--- src/components/Planets/Planets.js
+++ src/components/Planets/Planets.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 
 import tattoine from "../../assets/image/tattoine.png";
 import PlanetList from "./Planet/PlanetList";
@@ -14,7 +15,8 @@ class Planets extends Component {
     currentPage: 1,
     planets: [],
     selectedPlanet: [],
-    isLoaded: false,
+    planetsIsLoaded: false,
+    selectedPlanetIsLoaded: false,
   };
 
   getPlanets = async (page) => {
@@ -30,6 +32,7 @@ class Planets extends Component {
         nextPage: res.data.next,
         prevPage: res.data.previous,
         planets: res.data.results,
+        planetsIsLoaded: true,
       });
     });
     this.onPlanetSelect(1);
@@ -54,7 +57,7 @@ class Planets extends Component {
       console.log(res);
       this.setState({
         selectedPlanet: res.data,
-        isLoaded: true,
+        selectedPlanetIsLoaded: true,
       });
     });
   };
@@ -67,102 +70,99 @@ class Planets extends Component {
 
   render() {
     const maxPages = Math.ceil(this.state.count / 10);
-    // const PlanetsList = this.state.planets.slice(0, 5).map((planet, i) => {
-    //   return (
-    //     <div
-    //       className="planets-item"
-    //       key={i}
-    //       onClick={() => this.onPlanetSelect(planet.url.match(/(\d+)/)[0])}
-    //     >
-    //       {/* <Link to={`/react-star-wars/planet/${planet.url.match(/(\d+)/)[0]}`} className="card-link">
-    //                 </Link> */}
-    //       <img src={tattoine} alt={planet.name} />
-    //       <div className="preview">
-    //         <h2>{planet.name}</h2>
-    //         <p>{planet.climate}</p>
-    //       </div>
-    //     </div>
-    //   );
-    // });
     return (
       <div className="planets">
-        <div className="planets-preview">
-          <img src={tattoine} alt={this.state.selectedPlanet.name} />
-          <div className="planets-preview-desc">
-            <h2>{this.state.selectedPlanet.name}</h2>
-            <ul>
-              <li>Terrain: {this.state.selectedPlanet.terrain}</li>
-              <li>Diameter: {this.state.selectedPlanet.diameter}</li>
-              <li>Gravity: {this.state.selectedPlanet.gravity}</li>
-              <li>
-                Orbital Period: {this.state.selectedPlanet.orbital_period}
-              </li>
-              <li>
-                Rotation Period: {this.state.selectedPlanet.rotation_period}
-              </li>
-              <li>Surface Water: {this.state.selectedPlanet.surface_water}</li>
-              <li>Population: {this.state.selectedPlanet.orbital_period}</li>
-            </ul>
-            <div className="planets-preview-btn">
-              <Link
-                to={
-                  this.state.selectedPlanet.url
-                    ? `/react-star-wars/planet/${
-                        this.state.selectedPlanet.url.match(/(\d+)/)[0]
-                      }`
-                    : `/react-star-wars/planet/1`
-                }
-                className="trans-rt-lt"
-              >
-                Famous Residents
-              </Link>
+        {this.state.selectedPlanetIsLoaded ? (
+          <div className="planets-preview">
+            <img src={tattoine} alt={this.state.selectedPlanet.name} />
+            <div className="planets-preview-desc">
+              <h2>{this.state.selectedPlanet.name}</h2>
+              <ul>
+                <li>Terrain: {this.state.selectedPlanet.terrain}</li>
+                <li>Diameter: {this.state.selectedPlanet.diameter}</li>
+                <li>Gravity: {this.state.selectedPlanet.gravity}</li>
+                <li>
+                  Orbital Period: {this.state.selectedPlanet.orbital_period}
+                </li>
+                <li>
+                  Rotation Period: {this.state.selectedPlanet.rotation_period}
+                </li>
+                <li>
+                  Surface Water: {this.state.selectedPlanet.surface_water}
+                </li>
+                <li>Population: {this.state.selectedPlanet.orbital_period}</li>
+              </ul>
+              <div className="planets-preview-btn">
+                {typeof this.state.selectedPlanet.residents !== "undefined" &&
+                this.state.selectedPlanet.residents.length > 0 ? (
+                  <Link
+                    to={
+                      this.state.selectedPlanet.url
+                        ? `/react-star-wars/planet/${
+                            this.state.selectedPlanet.url.match(/(\d+)/)[0]
+                          }`
+                        : `/react-star-wars/planet/1`
+                    }
+                    className="trans-rt-lt"
+                  >
+                    Famous Residents
+                  </Link>
+                ) : null}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="planets-list">
-          <Link
-            to={`/react-star-wars/planets/${
-              this.state.currentPage > 1
-                ? +this.state.currentPage - 1
-                : this.state.currentPage
-            }`}
-          >
-            <div
-              className={
-                this.state.prevPage
-                  ? "planet-btn btn-prev"
-                  : "planet-btn btn-prev btn-disabled"
-              }
-              disabled={this.state.prevPage ? false : true}
-            ></div>
-          </Link>
-          {this.state.isLoaded ? (
+        ) : (
+          <div className="loader-container">
+            <Loader type="Circles" color="#00BFFF" height={80} width={80} />
+          </div>
+        )}
+        {this.state.planetsIsLoaded && this.state.selectedPlanetIsLoaded ? (
+          <div className="planets-list">
+            <Link
+              to={`/react-star-wars/planets/${
+                this.state.currentPage > 1
+                  ? +this.state.currentPage - 1
+                  : this.state.currentPage
+              }`}
+            >
+              <div
+                className={
+                  this.state.prevPage
+                    ? "planet-btn btn-prev"
+                    : "planet-btn btn-prev btn-disabled"
+                }
+                disabled={this.state.prevPage ? false : true}
+              ></div>
+            </Link>
+
             <PlanetList
               selectedPlanet={this.state.selectedPlanet.url.match(/(\d+)/)[0]}
               planets={this.state.planets}
               clicked={this.onPlanetSelect}
             />
-          ) : (
-            "Loading"
-          )}
 
-          <Link
-            to={`/react-star-wars/planets/${
-              this.state.currentPage < maxPages
-                ? +this.state.currentPage + 1
-                : this.state.currentPage
-            }`}
-          >
-            <div
-              className={
-                this.state.nextPage
-                  ? "planet-btn btn-next"
-                  : "planet-btn btn-next btn-disabled"
-              }
-              disabled={this.state.nextPage ? false : true}
-            ></div>
-          </Link>
-        </div>
+            <Link
+              to={`/react-star-wars/planets/${
+                this.state.currentPage < maxPages
+                  ? +this.state.currentPage + 1
+                  : this.state.currentPage
+              }`}
+            >
+              <div
+                className={
+                  this.state.nextPage
+                    ? "planet-btn btn-next"
+                    : "planet-btn btn-next btn-disabled"
+                }
+                disabled={this.state.nextPage ? false : true}
+              ></div>
+            </Link>
+          </div>
+        ) : (
+          <div className="loader-container">
+            <Loader type="BallTriangle" color="#00BFFF" height={80} width={80} />
+          </div>
+        )}
       </div>
     );
   }
