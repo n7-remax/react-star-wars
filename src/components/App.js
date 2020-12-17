1be3b92343d7a6afa06bcdba5aeab4e303dea608
n7-remax <ditritusa@gmail.com>
2020-12-17 17:45:32 +0200

    created homePage component for gh-pages routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,16 @@ import { Route } from "react-router-dom";
 import Planets from "./Planets/Planets";
 import FamousResidents from "./Planets/FamousResidents/FamousResidents";
 import Header from "./Header/Header";
+import HomePage from "./HomePage/HomePage";
 
 import "../style/style.scss";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Route exact path="/react-star-wars" component={HomePage}/>
       <Route exact path="/react-star-wars/planets/:page" component={Planets} />
       <Route
         exact
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <div className="video-background">
+        <div className="video-foreground">
+          <iframe
+            src="https://www.youtube.com/embed/xIl2z5wwjdA?controls=0&start=10&showinfo=0&rel=0&autoplay=1&loop=1&mute=1&playlist=xIl2z5wwjdA"
+            frameborder="0"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default HomePage;
