ecc37e9041fe610c4d702bc50084869cb2d93e9d
n7-remax <ditritusa@gmail.com>
2020-12-17 16:34:47 +0200

    change url in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import logo from "../../assets/image/logo.png";
 function Header() {
   return (
     <header className="header">
-      <Link to="/react-star-wars/">
+      <Link to="/react-star-wars/planets/1">
         <img src={logo} alt="star-wars-logo" className="logo" />
       </Link>
     </header>
