b49c1f1be5a0cea916cb44b746307aca5d5e22b3
n7-remax <ditritusa@gmail.com>
2020-12-21 17:24:48 +0200

    fixed iframe props

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,9 +4,10 @@ const HomePage = () => {
       <div className="video-background">
         <div className="video-foreground">
           <iframe
+            title="star-wars-bg"
             src="https://www.youtube.com/embed/xIl2z5wwjdA?controls=0&start=10&showinfo=0&rel=0&autoplay=1&loop=1&mute=1&playlist=xIl2z5wwjdA"
-            frameborder="0"
-            allowfullscreen
+            frameBorder="0"
+            allowFullScreen
           ></iframe>
         </div>
       </div>
