7033a571d7e122dbed548c7138a0ed26ef29a1f5
n7-remax <ditritusa@gmail.com>
2020-12-26 18:40:13 +0200

    added avatars to vehicles

--- src/components/Vehicles/Vehicle/Vehicle.js
+++ src/components/Vehicles/Vehicle/Vehicle.js
@@ -1,9 +1,18 @@
-import tattoine from "../../../assets/image/tattoine.png";
+import vehicleAvatars from "./vehicleAvatars";
 
 const Vehicle = (props) => {
+  const result = vehicleAvatars.find((avatar) => avatar.name === props.name);
+  console.log(result);
   return (
-    <div className={props.vehicleId === props.selectedVehicle? "component-item active": "component-item"} onClick={props.clicked}>
-      <img src={tattoine} alt={props.name} />
+    <div
+      className={
+        props.vehicleId === props.selectedVehicle
+          ? "component-item active"
+          : "component-item"
+      }
+      onClick={props.clicked}
+    >
+      {result ? <img src={result.url} alt={props.name} /> : null}
       <div className="preview">
         <h2>{props.name}</h2>
       </div>
@@ -11,5 +20,4 @@ const Vehicle = (props) => {
   );
 };
 
-
-export default Vehicle
+export default Vehicle;
---
+++ src/components/Vehicles/Vehicle/vehicleAvatars.js
@@ -0,0 +1,9 @@
+const vehicleAvatars = [
+    {
+      name: "Sand Crawler",
+      url:
+        "https://img1.pnghut.com/12/23/6/Jnp1Hw2iGT/yanoda-drawing-cartoon-jawa-art.jpg",
+    },
+]
+
+export default vehicleAvatars
