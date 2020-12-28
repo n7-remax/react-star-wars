7f9c05d9ea67c35cbafa3f080a06fd35ba4a5447
n7-remax <ditritusa@gmail.com>
2020-12-28 20:29:35 +0200

    added few vehicles avatar for testing

--- src/components/Vehicles/Vehicle/vehicleAvatars.js
+++ src/components/Vehicles/Vehicle/vehicleAvatars.js
@@ -2,7 +2,52 @@ const vehicleAvatars = [
     {
       name: "Sand Crawler",
       url:
-        "https://img1.pnghut.com/12/23/6/Jnp1Hw2iGT/yanoda-drawing-cartoon-jawa-art.jpg",
+        "https://lumiere-a.akamaihd.net/v1/images/databank_sandcrawler_01_169_55acf6cb.jpeg",
+    },
+    {
+      name: "T-16 skyhopper",
+      url:
+        "https://static.wikia.nocookie.net/rustarwars/images/4/45/Womp_Rats_Skyhopper.jpg/revision/latest/scale-to-width-down/1000?cb=20160709153434",
+    },
+    {
+      name: "X-34 landspeeder",
+      url:
+        "https://squir.com/media/catalog/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/4/7/47948.jpg",
+    },
+    {
+      name: "TIE/LN starfighter",
+      url:
+        "https://cdnb.artstation.com/p/assets/images/images/009/415/067/large/ben-armstrong-tie-ln-hangar-01.jpg",
+    },
+    {
+      name: "Snowspeeder",
+      url:
+        "https://i1.wp.com/thefwoosh.com/wp-content/uploads/2020/07/Black-Series-Snowspeeder-62.jpg",
+    },
+    {
+      name: "TIE bomber",
+      url:
+        "https://static.wikia.nocookie.net/rustarwars/images/1/17/TIE_Bomber_BF2.png/revision/latest?cb=20170822063124",
+    },
+    {
+      name: "AT-AT",
+      url:
+        "https://static.posters.cz/image/1300/%D0%9F%D0%BB%D0%B0%D0%BA%D0%B0%D1%82/star-wars-at-at-attack-i67847.jpg",
+    },
+    {
+      name: "AT-ST",
+      url:
+        "https://lumiere-a.akamaihd.net/v1/images/e6d_ia_5724_a150e6d4.jpeg?region=124%2C0%2C1424%2C802&width=960",
+    },
+    {
+      name: "Storm IV Twin-Pod cloud car",
+      url:
+        "https://i.pinimg.com/originals/62/e0/0d/62e00d1f888409ac4582f93389444042.jpg",
+    },
+    {
+      name: "Sail barge",
+      url:
+        "https://media.comicbook.com/2018/02/star-wars-jabbas-sail-barge-hasbro-crowdfund-1085664-1280x0.jpeg",
     },
 ]
 
